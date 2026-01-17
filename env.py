febf890d91f33c6b31aacb4b07e07cd28a210d37
sE7En <103624931+sE7En0908@users.noreply.github.com>
2026-01-17 22:35:31 +0800

    更新 env.py

--- env.py
+++ env.py
@@ -10,4 +10,7 @@ USER_INFO_URL = env['BASE_URL'] + '/api/v1/user/info'
 CONVERT_TRAFFIC_URL = env['BASE_URL'] + '/api/v1/user/checkIn'
 EMAIL = env['EMAIL']
 PASSWORD = env['PASSWORD']
-SERVER_KEY = env['SERVER_KEY']
+
+# Telegram 配置
+TG_BOT_TOKEN = env.get('TG_BOT_TOKEN')
+TG_USER_ID = env.get('TG_USER_ID')
